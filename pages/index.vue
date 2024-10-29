9a24f049e91d6c77b62d7924140457b286c580d3
DhitGT <adityadwisaputra666@gmail.com>
2024-10-29 14:28:12 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container class="flex flex-col items-center bg-gray-100 p-8">
+    <v-btn color="primary" class="mt-4">Vuetify Button</v-btn>
+    <div class="text-lg text-red-500">This is styled with Tailwind CSS</div>
+  </v-container>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
